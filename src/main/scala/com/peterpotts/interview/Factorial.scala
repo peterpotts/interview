7eb85aa564f38236383ba496ab2716f9d7aa5d31
Peter Potts <peter.potts@token.io>
2016-08-24 11:54:43 -0700

    Make factorial tailrec.

--- src/main/scala/com/peterpotts/interview/Factorial.scala
+++ src/main/scala/com/peterpotts/interview/Factorial.scala
@@ -1,5 +1,12 @@
 package com.peterpotts.interview
 
+import scala.annotation.tailrec
+
 object Factorial {
-  def apply(n: Long): Long = if (n == 0) 1 else n * apply(n - 1)
+  def apply(n: Long): Long = {
+    @tailrec def loop(left: Long, right: Long): Long =
+      if (right == 0) left else loop(left * right, right - 1)
+
+    loop(1, n)
+  }
 }
