17d7ca835c46103b883c52c5e06a49061d858090
Peter Potts <peter.potts@cointrust.com>
2016-08-28 08:44:21 -0700

    Connection pool.

---
+++ src/main/scala/com/peterpotts/interview/ConnectionPool.scala
@@ -0,0 +1,51 @@
+package com.peterpotts.interview
+
+trait Connection {
+  def write(text: String): Unit
+
+  def close(): Unit
+}
+
+class ConnectionFactory {
+  def createConnection: Connection =
+    new Connection {
+      private var open = true
+
+      def write(text: String): Unit = {
+        require(open)
+        println(text)
+      }
+
+      def close(): Unit = {
+        open = false
+      }
+    }
+}
+
+class ConnectionPool(factory: ConnectionFactory) {
+  private var free: List[Connection] = Nil
+
+  private def wrap(connection: Connection): Connection =
+    new Connection {
+      private var open = true
+
+      def write(text: String): Unit = {
+        require(open)
+        connection.write(text)
+      }
+
+      def close(): Unit = {
+        open = false
+        free = connection :: free
+      }
+    }
+
+  def getConnection: Connection =
+    free match {
+      case head :: tail =>
+        free = tail
+        wrap(head)
+      case Nil =>
+        wrap(factory.createConnection)
+    }
+}
