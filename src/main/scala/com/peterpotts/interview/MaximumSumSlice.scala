b0dbaedf22dc662b24fbaa9ba9560c8ebd3a5460
Peter Potts <peter.potts@cointrust.com>
2016-09-08 11:46:09 -0700

    Add max sum slice.

---
+++ src/main/scala/com/peterpotts/interview/MaximumSumSlice.scala
@@ -0,0 +1,24 @@
+package com.peterpotts.interview
+
+object MaximumSumSlice {
+  def simple(array: Array[Int]): Int = {
+    var sum = 0
+    var max = Int.MinValue
+
+    for (value <- array) {
+      sum += value
+      if (sum > max) max = sum
+      if (sum < 0) sum = 0
+    }
+
+    max
+  }
+
+  def fold(array: Array[Int]): Int =
+    array.foldRight((Int.MinValue, 0)) {
+      case (value, (max, sum)) =>
+        val foldMax = math.max(max, sum + value)
+        val foldSum = math.max(0, sum + value)
+        foldMax -> foldSum
+    }._1
+}
